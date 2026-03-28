2224366de4a7b6a566dcc23d1eda8ef61ac218c6
r14455009 <winche92221@gmail.com>
2026-03-28 13:18:04 +0800

    Create Mutect2.sh

---
+++ script/Mutect2.sh
@@ -0,0 +1,32 @@
+############################################
+# Calling variants by GATK Mutect2 #
+############################################
+echo "variant calling: start"
+echo "$(date '+%Y-%m-%d %H:%M:%S')"
+
+gatk Mutect2 \
+  -R /opt/ohpc/Taiwania3/pkg/biology/reference/Homo_sapiens/GATK/hg38/Homo_sapiens_assembly38.fasta \
+  -I ${DIR_ALN}/${sample}.sorted.markdup.bam \
+  -O ${sample}.sorted.markdup.M2.vcf.gz \
+  --bam-output ${sample}.sorted.markdup.M2.bam \
+  -L chr1 -L chr2 -L chr3 -L chr4 -L chr5 -L chr6 -L chr7 -L chr8 -L chr9  \
+  -L chr10 -L chr11 -L chr12 -L chr13 -L chr14 -L chr15 -L chr16 -L chr17  \
+  -L chr18 -L chr19 -L chr20 -L chr21 -L chr22
+
+echo "variant calling: Finished"
+echo "$(date '+%Y-%m-%d %H:%M:%S')"
+
+#####################################
+# Convert M2 bamout BAM to M2 CRAM  #
+#####################################
+echo "convert M2 bamout to cram: start"
+echo "$(date '+%Y-%m-%d %H:%M:%S')"
+
+samtools view -@ 2 -C -T ${ref} \
+  -o ${sample}.sorted.markdup.M2.cram \
+  ${sample}.sorted.markdup.M2.bam
+
+samtools index -@ 2 ${sample}.sorted.markdup.M2.cram
+
+echo "convert M2 bamout to cram: finished"
+echo "$(date '+%Y-%m-%d %H:%M:%S')"
